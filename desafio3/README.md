095278dde29571f461e576e48592fa25ab448a98
Fernanda Nagata Ito <fernanda.fn@gec.inatel.br>
2023-09-01 22:57:38 -0300

    desafio3

---
+++ desafio3/README.md
@@ -0,0 +1,35 @@
+# Desafio da aula 3
+Foi criado uma interface gráfica que permite os usuários insiram informações (peso e altura), realizem ações para calcular o IMC e limpar os campos (peso e altura).
+
+Ao calcular o IMC será mostrado o seu valor e também a sua classificação, de acordo com a tabela:
+
+| Intervalo de IMC | Classificação         |
+|-------------------|-----------------------|
+| Menos de 18.5     | Abaixo do peso        |
+| 18.5 - 24.9       | Peso ideal (parabéns) |
+| 25 - 29.9         | Levemente acima do peso|
+| 30 - 34.9         | Obesidade grau I       |
+| 35 - 39.9         | Obesidade grau II (severa)|
+| 40 ou mais        | Obesidade grau III (mórbida) |
+
+---
+### Meu ambiente
+Foi utilizado a IDE VSCode (OBS: Pode usar qualquer IDE).
+
+Para a instalação do Nodejs: https://nodejs.org/en 
+
+OBS: Foi utilizado a versão v18.16.0 do nodejs.
+
+---
+### Executando o código
+
+Para executar o código, vá ao diretório onde se encontra os arquivos app_imc e digite os seguintes comandos:
+
+1. Para instalar as dependências:
+```
+npm install
+```
+2. Para instalar as dependências:
+```
+npm run dev
+```
---
+++ desafio3/app_imc/src/App.svelte
@@ -0,0 +1,120 @@
+<script>
+
+	let peso = 0;
+	let altura = 0;
+	let imc = 0;
+	let classificacao;
+
+	function calcularIMC(){
+		if(peso > 0 && altura > 0){
+			imc = (peso / Math.pow(altura, 2)).toFixed(2);
+			if (imc <= 18.5) {
+				classificacao = 'abaixo do peso';
+			} else if (imc >= 18.6 && imc <= 24.9) {
+				classificacao = 'peso ideal (parabéns)';
+			} else if (imc >= 25 && imc <= 29.9) {
+				classificacao = 'levemente acima do peso';
+			} else if (imc >= 30 && imc <= 34.9) {
+				classificacao = 'obesidade grau I';
+			} else if (imc >= 35 && imc <= 39.9) {
+				classificacao = 'obesidade grau II (severa)';
+			} else {
+				classificacao = 'obesidade III (mórbida)';
+			}
+
+
+			document.getElementById("resultado").innerHTML = `Seu IMC é: ${imc}`
+			document.getElementById("complemento").innerHTML = `Sua classificação: ${classificacao}`
+			
+		} else {
+			imc = 0;
+			document.getElementById("resultado").innerHTML = `Valor(es) inválido(s)`
+		}
+
+
+	}
+
+	function limpar(){
+		peso = 0;
+		altura = 0;
+		document.getElementById("resultado").innerHTML = ``
+		document.getElementById("complemento").innerHTML = ``
+
+	}
+
+
+</script>
+
+<main>
+		<nav>
+			<h1>Calculadora de IMC</h1>
+			<!-- <button class="sobre"> Sobre o IMC </button> -->
+		</nav>
+
+		<div class="corpo">
+
+			<label>
+				Peso (kg): <input type="number" bind:value={peso}/>
+			</label>
+			<label>
+				Altura(m): <input type="number" bind:value={altura}/>
+			</label>
+			<button on:click={calcularIMC}> Calcular IMC </button>
+			<button on:click={limpar}> Novos valores </button>
+			<h3 id="resultado"> </h3>
+			<h3 id="complemento"> </h3>
+		</div>
+
+</main>
+
+<style>
+	*{
+		align-items: center;
+		text-align: center;
+		text-align: 2rem;
+	}
+	nav{
+		display: block;
+		align-items: center;
+		border-bottom: 2px solid #000;
+		margin: 0 400px;
+		text-align: center;
+	}
+	h1{
+		font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+		font-size: 1.5rem;
+
+	}
+	.corpo{
+		margin: 40px;
+	}
+	label{
+		display:block;
+		font-weight: bold;
+		margin-bottom: 0.5rem;
+	}
+	input{
+		text-align: left;
+		padding: 0.25rem;
+		border-radius: 5px;
+	}
+	button{
+		border-radius: 5px;
+		border-width: 2px;
+		background-color: orange;
+		font-weight: bold;
+		border-color: orange;
+		margin: 5px 10px;
+	}
+	button:hover{
+		background-color: rgb(255, 191, 71);
+		border-color: rgb(255, 191, 71);
+		cursor: pointer;
+	}
+	#resultado{
+		font-size: 1.2rem;
+	}
+	#complemento{
+		font-size: 1.2rem;
+	}
+</style>
